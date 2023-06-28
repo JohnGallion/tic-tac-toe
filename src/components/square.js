e95198b3e46c86f95472844abb2c8bf491763e68
John Gallion <johngallion@johns-air.attlocal.net>
2023-06-27 22:44:55 -0400

    a basic tic tac toe game

---
+++ src/components/square.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import './square.css'
+
+export default function Square({ value, onSquareClick }) {
+
+    
+
+   
+    return ( 
+    <button 
+    className="square"
+    onClick={onSquareClick}
+    >
+        {value}
+        </button>
+    );
+}
